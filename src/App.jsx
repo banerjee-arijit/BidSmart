474df8b1f6c3256d517a6c289c96d893d525a594
ArijitBanerjee <arijitbanerjee873@gmail.com>
2025-03-24 20:18:06 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,12 @@ import Navbar from "./components/Navbar";
 import { account } from "./lib/appwrite";
 
 const App = () => {
-  useEffect(() => {
-    const checkSession = async () => {
-      try {
-        const res = await account.get();
-        console.log(res);
-        console.log("User is logged in");
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    checkSession();
-  }, []);
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
 };
 
 export default App;
