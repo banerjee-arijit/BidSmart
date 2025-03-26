e743bed6b84d0f702aa4a8dfb52eed16e0a6353b
ArijitBanerjee <arijitbanerjee873@gmail.com>
2025-03-26 20:39:10 +0530

    test

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -35,7 +35,7 @@ const Dashboard = () => {
       <BGanimation />
       <div className="relative z-10 flex flex-col lg:flex-row h-screen">
         {/* Sidebar */}
-        <div className="w-full lg:w-64 bg-black overflow-hidden overflow-y-scroll">
+        <div className="w-full lg:w-64 bg-black overflow-scroll md:overflow-hidden">
           <Sidebar />
         </div>
 
